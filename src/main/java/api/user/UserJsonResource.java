e62529e7931d4f5e0bb5a9438f7cab84cb52f018
David <david-axel-wendbenedo.nikyema.1@ulaval.ca>
2018-08-07 18:44:01 -0400

    adding travis to the project

--- src/main/java/api/user/UserJsonResource.java
+++ src/main/java/api/user/UserJsonResource.java
@@ -14,4 +14,5 @@ public class UserJsonResource {
 
 		userService.create(userDto);
 	}
+
 }
---
+++ src/main/java/server/StartUPServer.java
@@ -0,0 +1,18 @@
+package server;
+
+import org.eclipse.jetty.server.Server;
+
+import java.util.logging.Logger;
+
+public class StartUPServer {
+
+	private static final Logger SERVER_LOGGER = Logger.getLogger(StartUPServer.class.getName());
+
+	private  Server server;
+
+	public static void main (String[] args )throws Exception {
+
+
+
+	}
+}
